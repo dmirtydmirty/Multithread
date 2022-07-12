c6dbab921233e8f2495207dc7714d05112abb24f
AIDmitry <golub.plumbum@gmail.com>
2022-07-12 03:22:28 +0300

    Исправления

--- Lib.cpp
+++ Lib.cpp
@@ -1,15 +1,15 @@
 #include "Lib.h"
-long freq = 500;
+long freq = 50;
 
 std::ofstream fout;
 std::mutex mtx;
 std::condition_variable cond;
 unsigned counter=0;
-unsigned char writePosition=0;
+int writePosition=0;
 long writeIndex=0;
 std::queue<elementOfQueue> queueOfdData;
 
-unsigned* bufer = new unsigned[256*1024*1024/sizeof(unsigned)];
+unsigned* buffer = new unsigned[256*1024*1024/sizeof(unsigned)];
 
 elementOfQueue::elementOfQueue(unsigned * ptr, long s)
 {
@@ -21,16 +21,17 @@ long elementOfQueue::GetSize(){return this->size;}
 
 void GenerateData()
 {
-    while (1)
+    for (int j=0; j<32000 ; j++)
     {
-
+        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
         for(long i=0; i<1024*1024/sizeof(unsigned); i++)
         {
-            *(bufer+writeIndex)=counter++;
+            *(buffer+writeIndex)=counter++;
             writeIndex++;
         }
+        writeIndex=writeIndex % (256*1024*1024/sizeof(unsigned));
 
-        elementOfQueue oneMb(bufer+writePosition, 1024*1024/sizeof(unsigned));
+        elementOfQueue oneMb(buffer+writePosition*1024*1024/sizeof(unsigned), 1024*1024/sizeof(unsigned));
         std::unique_lock<std::mutex> lock(mtx);
         if(queueOfdData.size()>=256)
         {
@@ -40,8 +41,11 @@ void GenerateData()
         queueOfdData.push(oneMb);
         cond.notify_all();
         lock.unlock();
-        writeIndex=1024*1024/sizeof(unsigned)*(++writePosition);
-        std::this_thread::sleep_for(std::chrono::microseconds (100000/freq));
+        writePosition++;
+        writePosition = writePosition % (1024*1024/sizeof(unsigned)*256);
+        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+        std::this_thread::sleep_for(std::chrono::microseconds (100000/freq - duration));
     }
 
 }
@@ -53,23 +57,26 @@ void PrintData()
 
     while (1)
     {
-        double start = clock();
+        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
         std::unique_lock<std::mutex> lock(mtx);
         cond.wait(lock, [] {
             return !queueOfdData.empty();
         });
 
 
-        fout.open("Data.txt", std::ofstream::app);
+        fout.open("Data.txt", std::ios::binary | std::ios::app);
         if (fout.is_open())
         {
             for(long i=0; i<queueOfdData.front().GetSize(); i++)
-                fout<< *(queueOfdData.front().GetPosition()+i);
+                fout<< std::bitset<32>(*(queueOfdData.front().GetPosition()+i));
             queueOfdData.pop();
 
             fout.close();
         }
         lock.unlock();
-        std::cout << 1/((clock() - start) / CLOCKS_PER_SEC) << " MB/S"<< std::endl;
+        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+        float duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+        std::cout << 1000000/duration << " MB/S" << std::endl;
+
     }
 }

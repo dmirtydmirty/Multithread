74dedc843579b913585667a000847ebd293d7504
Антонов Дмитрий Игоревич <107511435+dmirtydmirty@users.noreply.github.com>
2024-12-01 19:17:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main() {
 
     unsigned * buffer = new unsigned[bufferSizeMb*oneMbSize];
     int frequency=60000; // 1/s
-   std::thread th1(GenerateData, frequency, buffer);
+    std::thread th1(GenerateData, frequency, buffer);
     std::thread th2(PrintData);
     th1.join();
     th2.join();
